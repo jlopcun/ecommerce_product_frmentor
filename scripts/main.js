1b04cc92adfb921cd951b504ca221eee47288484
jlopcun <62543437+jlopcun@users.noreply.github.com>
2022-06-05 13:09:13 +0200

    adding a lot of things to say, sorry :(

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,24 @@
 import { openPhoneMenu } from "./menu.js"
-
+import { moveSlide,openAlert } from "./slider.js"
 const id = (el) => document.getElementById(el)
 
 const $openMenu = id('openMenu'),
-$menu = id('menu')
+$menu = id('menu'),
+$slideButtons = id('slideButtons'),
+$thumbnails = id('thumbnails'),
+slideImage = id('slideImage'),
+$slideAlertButtons = id('slideAlertButtons'),
+$slideAlertImage = id('slideAlertImage'),
+$thumbnailsAlert = id('thumbnailsAlert'),
+$slideAlert = id('slideAlert'),
+$closeAlert = id('closeAlert')
 
 window.addEventListener('DOMContentLoaded',()=>{
     $openMenu.addEventListener('click',()=>openPhoneMenu($openMenu,$menu))
+    $slideButtons.addEventListener('click',(e)=>moveSlide(e.target,slideImage))
+    $thumbnails.addEventListener('click',(e)=>moveSlide(e.target,slideImage))
+    $slideAlertButtons.addEventListener('click',(e)=>moveSlide(e.target,$slideAlertImage))
+    $thumbnailsAlert.addEventListener('click',(e)=>moveSlide(e.target,$slideAlertImage))
+    slideImage.addEventListener('click',()=>openAlert($slideAlert))
+    $closeAlert.addEventListener('click',()=>openAlert($slideAlert))
 })
---
+++ scripts/slider.js
@@ -0,0 +1,60 @@
+export {moveSlide,openAlert}
+
+const id = (el) => document.getElementById(el)
+
+const moveSlide = (target,mainImage) =>{
+    const direction = target.dataset.direction || target.parentNode.dataset.direction
+    if(direction){
+        const src = mainImage.src,
+        actualImage = src.substring(src.length-5,src.length-4)
+    const actions = {
+        'right':()=>{
+            mainImage.classList.add('Sliding');
+            
+            setTimeout(()=>{
+                (Number(actualImage)+1 === 5)
+            ?mainImage.src = `http://127.0.0.1:5500/assets/images/image-product-${1}.jpg`
+            :mainImage.src = `http://127.0.0.1:5500/assets/images/image-product-${Number(actualImage)+1}.jpg`
+            },300)
+
+            setTimeout(()=>{
+                mainImage.classList.remove('Sliding');
+            },500)
+        },
+        'left':()=>{
+            mainImage.classList.add('Sliding');
+
+            setTimeout(()=>{
+                (Number(actualImage)-1 === 0)
+            ?mainImage.src = `http://127.0.0.1:5500/assets/images/image-product-${4}.jpg`
+            :mainImage.src = `http://127.0.0.1:5500/assets/images/image-product-${Number(actualImage)-1}.jpg`
+            },300)
+
+            setTimeout(()=>{
+                mainImage.classList.remove('Sliding');
+            },500)
+            
+        }
+    }
+    actions[direction]()
+    }
+    else if(target.className.includes('thumbnail')){
+        
+        const src = target.src,
+        imgToDisplay = src.substring(src.length-15,src.length-14)
+        mainImage.classList.add('Sliding');
+        
+        setTimeout(()=>{
+            mainImage.src = `http://127.0.0.1:5500/assets/images/image-product-${imgToDisplay}.jpg`
+        },300)
+
+        setTimeout(()=>{
+            mainImage.classList.remove('Sliding')
+        },500)
+    }
+    
+}
+
+const openAlert = (appear) =>{
+    appear.classList.toggle('CLOSED')
+}
